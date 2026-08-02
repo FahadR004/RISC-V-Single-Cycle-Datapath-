ffe4d2ca9c4e9430d3e50b1e83dc5cedba08e21f
FahadR004 <fahad.rasheed.004@gmail.com>
2026-08-02 22:50:26 +0500

    This is Single Cycle. Removed pipeline additions of pc_write

--- Fetch/PC.sv
+++ Fetch/PC.sv
@@ -3,7 +3,6 @@ module PC_Register #(
 ) (
     input clk,
     input rst_n,
-    input pc_write, // From Hazard Unit
     input [ADDR_WIDTH-1:0] next_pc, // Next PC value (Byte Address)
     output reg [ADDR_WIDTH-1:0] pc_current // Current PC value (Byte Address)
 );
@@ -11,10 +10,8 @@ module PC_Register #(
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n)
             pc_current <= {ADDR_WIDTH{1'b0}};  // Reset 
-        else if (pc_write)
-            pc_current <= next_pc;  // Update value to next address     
         else
-            pc_current <= pc_current;  // Hold current address            
+            pc_current <= next_pc;  // Update to next PC value            
     end
 
 endmodule
